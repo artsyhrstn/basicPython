dfc1a01261687510f2772ac1eb6a5c8713a699ac
artsyhrstn <107185406+artsyhrstn@users.noreply.github.com>
2023-06-27 22:26:05 +0700

    Add files via upload

---
+++ matplotlib/matplotlib1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+"""
+    1. Membuat Data
+    2. Membuat plot
+    3. Menampilkan Plot
+"""
+
+# 1 membuat data
+x = np.array([1,2,3,4,5])
+y = np.array([1,4,9,16,25])
+
+# 2 Membuat plot
+plt.plot(x,y)
+
+# 3 Menampilkan plot
+plt.show
