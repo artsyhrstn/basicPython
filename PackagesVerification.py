a4f60fa260e82d43ad2094e77fd4b6d744c60fa6
Hisbi Asyihristani <107185406+artsyhrstn@users.noreply.github.com>
2024-02-15 11:59:37 +0700

    Create PackagesVerification.py

---
+++ PackagesVerification.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+print("Tensorflow Version is", tf.__version__)
+
+import pandas as pd
+print("Pandas Version is", pd.__version__)
+
+import numpy as np
+print("Numpy Version is", np.__version__)
+
+import scipy
+print("Scipy Version is", scipy.__version__)
+
+import PIL
+print("Tensorflow Datasets Version is", PIL.__version__)
+
+import tensorflow_datasets as tfds
+print("Tensorflow Datasets Version is", tfds.__version__)
